16ce53c86f121d2045f250299321dafcfe30deaa
Edoardo <edoardo.disabatino@jwt.com>
2019-03-26 17:44:26 +0100

    INIT Vue-Colors Test

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <img src="./assets/img/logo.png">
+    <h1>{{ msg }}</h1>
+    <h2>{{colorName}}</h2>
+    <buttonComponent v-bind:hex="red" label="lollone" @BUTTON_CLICK="pickColor"></buttonComponent>
+    <buttonComponent v-bind:hex="green" label="asdone" @BUTTON_CLICK="pickColor"></buttonComponent>
+    <buttonComponent v-bind:hex="blue" label="patatone" @BUTTON_CLICK="pickColor"></buttonComponent>
+  </div>
+</template>
+
+<script>
+
+import buttonComponent from './components/button.vue';
+
+export default {
+
+  components:{
+    buttonComponent
+  },
+
+  name: 'app',
+  data () {
+    return {
+      msg: 'TEST COLORS',
+      colorName: "select color",
+      red:'#ff0000',
+      green:'#00ff00',
+      blue:'#0000ff'
+    }
+  },
+
+  methods:{
+    pickColor: function(payload){
+      this.colorName = payload;
+      console.log(payload)
+
+    }
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/button.vue
@@ -0,0 +1,27 @@
+<template>
+<button @click="pickColor" v-bind:style={color:hex}>
+    {{label}}
+</button>
+</template>
+
+<script>
+export default {
+
+    props:["hex","label"],
+
+    data: function() {
+        return {
+            // text: this.hex
+            //text: "Click"
+        }
+    },
+
+    methods:{
+        pickColor: function(){
+            var colorHex = this.hex;
+            this.$emit("BUTTON_CLICK", colorHex);
+        }
+    }
+}
+
+</script>
